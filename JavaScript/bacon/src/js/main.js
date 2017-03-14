eceffd7efb7a17be14939a1d654f6a5df7fca99c
nacam403 <satnakam@gmail.com>
2017-03-14 22:41:13 +0900

    ストリームとプロパティの初歩。

--- JavaScript/bacon/src/js/main.js
+++ JavaScript/bacon/src/js/main.js
@@ -8,4 +8,22 @@ $(() => {
   // const clickStream = Bacon.fromEvent(document.getElementById('header'), 'click');
 
   clickStream.onValue(() => console.log('clicked!'));
+
+  // ボタンをクリックすると現在時刻が流れてくるストリーム。
+  const buttonAStream = $('#buttonA').asEventStream('click').map(() => new Date().getTime());
+  buttonAStream.onValue((value) => console.log(`buttonA: ${value}`));
+
+  // flatMapを使うと、ストリームに値が流れてくるたびに、値に応じて新しいストリームが作られる。
+  buttonAStream.flatMap((value) => Bacon.fromArray([value]))
+    .onValue(() => console.log('createdStream.onValue()'));
+
+  // ストリームに流れてきた最新の値をプロパティとして受ける。
+  const buttonAProperty = buttonAStream.toProperty(0);
+  // プロパティの値をHTMLに割り当てる。
+  buttonAProperty.assign($('#textA'), 'text');
+
+  // ボタンBを押すたびに、その時のプロパティAの値が流れてくるストリームが作られる。
+  const buttonBStream = $('#buttonB').asEventStream('click');
+  const sampledStream = buttonAProperty.sampledBy(buttonBStream);
+  sampledStream.log('sampledStream');
 });
