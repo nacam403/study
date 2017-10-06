618eaeef9778f9609fabaf0a93ed1704d01a2b89
nacam403 <satnakam@gmail.com>
2017-10-06 23:55:13 +0900

    初めてのReact

--- JavaScript/npm-scripts-study/src/js/main.js
+++ JavaScript/npm-scripts-study/src/js/main.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import lib from './lib';
 
-const main = () => {
-  const h1 = document.getElementsByTagName('h1')[0];
-  h1.textContent = lib.message;
+const h1 = document.getElementsByTagName('h1')[0];
+h1.textContent = lib.message;
+
+const Hello = (props) => {
+  return (
+    <div onClick={() => alert('Hello!')}>Hello {props.name} !</div>
+  );
 };
 
-main();
+ReactDOM.render(<Hello name="React"/>, document.getElementById('root'));
