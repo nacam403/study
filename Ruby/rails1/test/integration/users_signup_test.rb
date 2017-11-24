d62a64a8477adda175ba8d2c5815d35a3733f363
nacam403 <satnakam@gmail.com>
2017-11-25 00:34:17 +0900

    7章（ユーザー登録）

---
+++ Ruby/rails1/test/integration/users_signup_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "aaa",
+          password: "1",
+          password_confirmation: "2"
+        }
+      }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { 
+        user: { 
+          name:  "Example User",
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
