70fafed79b6c59582078465083ae7709cbb67c5f
nacam403 <satnakam@gmail.com>
2017-11-26 13:22:18 +0900

    9章　ログイン状態の永続的な保持

--- Ruby/rails1/app/controllers/sessions_controller.rb
+++ Ruby/rails1/app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     session = params[:session]
-    user = User.find_by(email: session[:email].downcase)
-    if user && user.authenticate(session[:password])
-      log_in user
-      redirect_to user
+    @user = User.find_by(email: session[:email].downcase)
+    if @user && @user.authenticate(session[:password])
+      log_in @user
+      session[:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    logout
+    logout if logged_in?
     redirect_to root_url
   end
 end
