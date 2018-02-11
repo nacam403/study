43e2b4200dddef6da10ccbbe42178b96b0abdf1a
nacam403 <satnakam@gmail.com>
2018-02-11 21:54:45 +0900

    3章

--- Python/DeepLearningFromScratch/neural_network.py
+++ Python/DeepLearningFromScratch/neural_network.py
@@ -48,3 +48,15 @@ def forward(network, x):
 # x = np.array([1.0, 0.5])
 # y = forward(network, x)
 # print(y)
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+# a = np.array([0.3, 2.9, 4.0])
+# y = softmax(a)
+# print(y)
+# print(np.sum(y))
